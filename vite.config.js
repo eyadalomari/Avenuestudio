c9851759148cc5dd359771d60ac240d9c1594ba0
Eyad Amin <e.alameen@sakan.co>
2024-05-19 12:05:46 +0300

    js and css

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,15 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+
+                'resources/assets/css/styles.min.css',
+                'resources/assets/libs/jquery/dist/jquery.min.js',
+                'resources/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js',
+                'resources/assets/js/sidebarmenu.js',
+                'resources/assets/js/app.min.js',
+                'resources/assets/libs/simplebar/dist/simplebar.js',
+                'resources/assets/libs/apexcharts/dist/apexcharts.min.js',
+                'resources/assets/js/dashboard.js',
             ],
             refresh: true,
         }),
